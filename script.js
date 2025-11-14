d37291c0a308d4bac286d6df64d649a1c1338ff5
mikebru <32722658+mikebru@users.noreply.github.com>
2025-11-14 15:36:29 -0600

    adding in the long fall

--- script.js
+++ script.js
@@ -13,6 +13,7 @@ function loadHeader() {
                 <li class="dropdown">
                     <a href="#" class="dropdown-toggle">Projects</a>
                     <ul class="dropdown-menu">
+                        <li><a href="${isProjectPage ? '../projects/longFall.html' : 'projects/longFall.html'}" class="project-link" data-project="longFall">The Long Fall</a></li>
                         <li><a href="${isProjectPage ? '../projects/memorii.html' : 'projects/memorii.html'}" class="project-link" data-project="memorii">Memor-ii</a></li>
                         <li><a href="${isProjectPage ? '../projects/awry-awash.html' : 'projects/awry-awash.html'}" class="project-link" data-project="awry-awash">Awry Awash</a></li>
                         <li><a href="${isProjectPage ? '../projects/39-inside.html' : 'projects/39-inside.html'}" class="project-link" data-project="39-inside">39 Inside</a></li>
